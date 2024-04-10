d020668933ab1752f971c93434834bb25beea687
babayaga88 <zeepearson412@gmail.com>
2024-04-09 21:05:12 -0400

    ss

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from backend import jsonfilehandler
-from blackjack import gamehandler
-import matplotlib.pyplot as plt 
-import numpy as np
-
-jsonHandler = jsonfilehandler.JsonFileHandler()
-gameHandler = gamehandler.GameHandler()
 
 
-def user_account_interface():
-    user_name = input('Enter full name: ').lower()
-    user_email = input('Enter an email address: ').lower()
-    user_id = input('Create a username: ')
-    user_password = input('Create a password: ')
-    jsonHandler.add_json(user_id,user_name,user_email,user_password,0,0,0,0)
-    
-    
